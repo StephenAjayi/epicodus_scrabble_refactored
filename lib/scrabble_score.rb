e61dd5ab6c2b68341ecf80bf20c4292feeac800f
stephen & Jessi <student@epicodus.com>
2015-05-28 11:25:45 -0700

    add spec and code to scrabble_score.rb to return score for multiple letters, all specs passing.

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,6 +1,6 @@
 class String
   define_method(:scrabble_score) do
-    letter = self
+    word_letters = self.split("")
 
     score = 0
 
@@ -8,7 +8,9 @@ class String
       "l" => 1, "m" => 3, "n" => 1, "o" => 1, "p" => 3, "q" => 10, "r" => 1, "s" => 1, "t" => 1, "u" => 1, "v" => 4,
       "w" => 4,  "x" => 8, "y" => 4, "z" => 10}
 
-      score = score.+(letter_values.fetch(letter))
-      score
+    word_letters.each() do |word_letter|
+      score = score.+(letter_values.fetch(word_letter))
     end
+    score
   end
+end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,10 +1,15 @@
 require('rspec')
 require('scrabble_score')
+require('pry')
 
 describe('scrabble_score') do
 
   it('returns the numerical value of a letter') do
     expect("w".scrabble_score()).to(eq(4))
+  end
+
+  it('returns the numerical value of multiple letters') do
+    expect("happy".scrabble_score).to(eq(15))
   end
 
 end
